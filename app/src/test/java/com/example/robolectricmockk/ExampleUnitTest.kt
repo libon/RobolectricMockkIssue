0775339da1e8661345bf026dab93a0e59f819b07
Carmen Alvarez <calvarez@libon.com>
2020-08-12 16:38:14 +0200

    Add robolectric and reproduce the failing test scenario

--- app/src/test/java/com/example/robolectricmockk/ExampleUnitTest.kt
+++ app/src/test/java/com/example/robolectricmockk/ExampleUnitTest.kt
@@ -1,18 +1,37 @@
 package com.example.robolectricmockk
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import io.mockk.mockk
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+@RunWith(AndroidJUnit4::class)
 class ExampleUnitTest {
+    private lateinit var foo: Foo
+
+    @Before
+    fun setUp() {
+        foo = mockk(relaxed = true)
+    }
+
     @Test
-    fun testMockk() {
-        val foo = mockk<Foo>(relaxed=true)
+    fun testMockk1() {
+        assertEquals(0, foo.hello.length)
+    }
+
+    @Config(sdk=[28])
+    @Test
+    fun testMockk2() {
+        assertEquals(0, foo.hello.length)
     }
 }
